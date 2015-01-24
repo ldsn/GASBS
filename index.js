014b25bf947db24a71ae53596c1e8e7fbd778e2a
Secbone <secbone@gmail.com>
2015-01-24 16:49:45 +0800

    太阳出来我爬山坡，爬到了山坡我想唱歌

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+var request = require("request");
+
+var appid = "wx053e3fbce4850bed";
+var secret = "604221418f269f22f9a2220c5291a59c";
+
+var tokenURL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+appid+"&secret="+secret;
+var apiURL = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=";
 
 server.listen(9090);
+
+request(tokenURL, function(error, response, body) {
+    var result = JSON.parse(body);
+    request(apiURL+result.access_token, function(err, res, data) {
+        console.log(data);
+    });
+});
 
 app.use("/", express.static( __dirname + "/html" ));
 
